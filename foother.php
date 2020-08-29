7cf9cf339550ba21aec37c48904e871e1197609c
suzumiya-work <suzumiya.work@gmail.com>
2020-08-29 17:20:53 +0900

    ヘッダーとフッダーを追加

---
+++ foother.php
@@ -0,0 +1,8 @@
+<hr>
+<footer class="site-footer">
+    <p><?php bloginfo('name') ?></p>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 
+get_header();
+
 if ( have_posts() ) :
     while ( have_posts() ) : the_post(); ?>
 
-        <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
-        <?php the_content() ?>
+    <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+    <?php the_content() ?>
 
     <?php endwhile;
 
@@ -12,5 +14,7 @@ else :
     echo '<p>There are no posts!</p>';
 
 endif;
+
+get_footer();
 
 ?>
