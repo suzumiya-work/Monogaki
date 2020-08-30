7d801a7ac3a56fceca2f2a84acb1faf09b1d6054
suzumiya-work <suzumiya.work@gmail.com>
2020-08-30 13:00:07 +0900

    css周りを改善

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
 
 function custom_theme_assets() {
-  wp_enqueue_style( 'style', get_stylesheet_uri() );
+    wp_enqueue_style('ress', get_stylesheet_uri().'/ress.css');
+    wp_enqueue_style('style', get_stylesheet_uri());
 }
 
 add_action( 'wp_enqueue_scripts', 'custom_theme_assets' );
