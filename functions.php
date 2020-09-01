e75fb11994ffb16c48eddb43aa5d7348cde44039
suzumiya-work <suzumiya.work@gmail.com>
2020-09-01 21:15:23 +0900

    Add functions.php to font.css

--- functions.php
+++ functions.php
@@ -1,8 +1,9 @@
 <?php
 
 function custom_theme_assets() {
-    wp_enqueue_style('ress', get_stylesheet_uri().'/ress.css');
-    wp_enqueue_style('style', get_stylesheet_uri());
+    wp_enqueue_style('ress', get_template_directory_uri().'/ress.css', array(), null, 'all');
+    wp_enqueue_style('fonts', get_template_directory_uri().'/fonts.css', array(), null, 'all');
+    wp_enqueue_style('style', get_template_directory_uri().'/style.css', array(), null, 'all');
 }
 
 add_action( 'wp_enqueue_scripts', 'custom_theme_assets' );
