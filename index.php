fdf5db4b227cc3c7f0cd52e14559628f4349a9ab
suzumiya-work <suzumiya.work@gmail.com>
2020-09-03 20:27:55 +0900

    整理

--- index.php
+++ index.php
@@ -1,23 +1,19 @@
-<?php
+<?php get_header(); ?>
+<main>
+    <?php if ( have_posts() ) :
+        while ( have_posts() ) : the_post(); ?>
 
-get_header();
+        <article class="post">
+            <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+            <p><?php the_time("Y年n月j日"); ?></p>
+            <?php the_excerpt(); ?>
+        </article>
 
-if ( have_posts() ) :
-    while ( have_posts() ) : the_post(); ?>
+        <?php endwhile;
 
-    <article class="post">
-        <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
-        <p><?php the_time("Y年n月j日"); ?></p>
-        <?php the_excerpt(); ?>
-    </article>
+    else :
+        echo '<p>There are no posts!</p>';
 
-    <?php endwhile;
-
-else :
-    echo '<p>There are no posts!</p>';
-
-endif;
-
-get_footer();
-
-?>
+    endif; ?>
+</main>
+<?php get_footer(); ?>
