3bade6346448bcacd1e2ddb9c62a1741bf203021
suzumiya-work <suzumiya.work@gmail.com>
2020-09-06 14:48:03 +0900

    ヘッダーメニューを追加

--- functions.php
+++ functions.php
@@ -5,5 +5,9 @@ function custom_theme_assets() {
     wp_enqueue_style('fonts', get_template_directory_uri().'/fonts.css', array(), null, 'all');
     wp_enqueue_style('style', get_template_directory_uri().'/style.css', array(), null, 'all');
 }
-
 add_action( 'wp_enqueue_scripts', 'custom_theme_assets' );
+
+function register_my_menu() {
+    register_nav_menu('header-menu',__( 'ヘッダーメニュー' ));
+}
+add_action( 'init', 'register_my_menu' );
--- header.php
+++ header.php
@@ -3,13 +3,19 @@
 
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
   <title><?php bloginfo('name');?> | <?php bloginfo('description');?></title>
   <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div class="container">
-  <header class="site-header">
-    <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
-    <h4><?php bloginfo('description'); ?></h4>
+<header class="site-header">
+    <div>
+        <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+        <?php wp_nav_menu(array(
+            'theme_location' => 'header-nav',
+            'container' => 'nav'
+        ));?>
+    </div>
 </header>
