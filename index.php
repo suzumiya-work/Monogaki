2ecad798f0f9eaa9887962d7da809a6c4010c433
suzumiya-work <suzumiya.work@gmail.com>
2020-09-06 18:47:03 +0900

    ページ切り替え機能とヘッダーのメニューを追加

--- index.php
+++ index.php
@@ -4,16 +4,26 @@
         while ( have_posts() ) : the_post(); ?>
 
         <article class="post">
-            <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
-            <p class="date"> - <?php the_time("Y年n月j日"); ?></p>
+            <div class="title">
+                <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+                <p class="date"> - <?php echo get_the_date(); ?></p>
+            </div>
             <?php the_excerpt(); ?>
         </article>
 
         <?php endwhile;
-
+    
     else :
         echo '<p>There are no posts!</p>';
 
     endif; ?>
+    <nav class="pagination">
+    <?php echo paginate_links( array(
+        'type' => 'list',
+        'mid_size' => '2',
+        'prev_text' => '<',
+        'next_text' => '>'
+        )); ?>
+    </nav>
 </main>
 <?php get_footer(); ?>
